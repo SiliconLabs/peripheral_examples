0b706e7d1da283f3d8e2a8df73b2d027479c8303
Brian Lampkin <Brian.Lampkin@silabs.com>
2019-09-30 10:24:00 -0500

    Merge pull request #29 in PA_MCU_EXTERNAL_PROJECTS/mcu_peripheral_examples from feature/gpio_prs_ldma_usart_example to master
    Squashed commit of the following:
    
    commit 3717b03915b5db0c54829e0d5dc37b7b14402dc3
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Fri Aug 2 14:13:15 2019 -0500
    
        renaming example folder to match naming scheme
    
    commit 486a3fcb974de9defc96350d8f0283edb884f289
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Fri Aug 2 14:11:15 2019 -0500
    
        fixing comments in c files
    
    commit 8fa975058a1e294a2bc2901b54bef58045c53ce5
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Fri Aug 2 09:06:21 2019 -0500
    
        fixing build error
    
    commit fd9a7854441880359123c8be88e2c0eb569a5b7d
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Thu Aug 1 14:30:37 2019 -0500
    
        specifying pin numbers for EFR boards
    
    commit 446b79285ced2133dd9e03b241e266c39472a83f
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Thu Aug 1 12:00:41 2019 -0500
    
        fixing BUGS
    
    commit b3aa600decc1e1407c91f8ad5c313ff065df7b92
    Author: jojin <Joseph.Jin@silabs.com>
    Date:   Thu Aug 1 10:26:42 2019 -0500
    
        adding example that uses the gpio, prs, ldma and usart periperals
    
        the prs is configured to send a signal to the ldma when a particular gpio pin changes voltage. Upon receiving the signal, the ldma, using a series of descriptors, will trigger the usart to send several bytes of data. SINCE that the ldma writes data faster then the usart can send data, the input source of the prs is switched from the gpio pin to the usart0 txc flag once the first byte is transfered to prevent the ldma from overwriting bytes in the usart FIFO which are yet to be sent.

---
+++ series1/usart/gpio_prs_ldma_usart/readme_s1.txt
@@ -0,0 +1,115 @@
+gpio_prs_ldma_usart
+
+This example demonstrates using LDMA with PRS where USART
+packet transfers are triggered every push of Push Button 0.
+
+7 bytes, are transmitted on USART TX Pin.
+
+How To Test:
+1. Build the project and download to the Starter Kit
+2. Connect board to CP2102, or any usb to uart bridge controller (for CP2102, connect the board's USART TX pin to RXD and the USART RX pin to TXD).
+3. Read serial data from the controller using a terminal emulator like putty.
+4. Every press of Push Button 0 should cause 7 byte of text to be transmitted from the usart.
+Observe usart transmissions in the terminal emulator.
+
+Peripherals Used:
+USART0 - Asynchronous
+PRS
+LDMA
+GPIO
+
+Board: Silicon Labs EFM32GG11 Starter Kit (SLSTK3701A)
+Device: EFM32GG11B820F2048GL192
+PC0 - USART0 TX - EXP Header Pin 16
+PC1 - USART0 RX - EXP Header Pin 15
+PC8 - Push Button PB0
+
+Board: Silicon Labs EFM32PG1B Starter Kit (SLSTK3401A)
+Device: EFM32PG1B200F256GM48
+PA0 - USART0 TX - EXP Header Pin 12
+PA1 - USART0 RX - EXP Header Pin 14
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32MG1P Starter Kit (BRD4151A)
+Device: EFR32MG1P232F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32BG1 Starter Kit (BRD4100A)
+Device: EFR32BG1P232F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32FG1 Starter Kit (BRD4251B)
+Device: EFR32FG1P133F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFM32PG12 Starter Kit (SLSTK3402A)
+Device: EFM32PG12B500F1024GL125
+PA0 - USART0 TX
+PA1 - USART0 RX
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32MG12 Starter Kit (BRD4001A)
+Device: EFR32MG12P433F1024GL125
+PA0 - USART0 TX - P33
+PA1 - USART0 RX - P34
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32BG12 Starter Kit (BRD4103A)
+Device: EFR32BG12P332F1024GL125
+PA0 - USART0 TX - P33
+PA1 - USART0 RX - P34
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32FG12 Starter Kit (BRD4253A)
+Device: EFR32FG12P433F1024GL125
+PA0 - USART0 TX - P33
+PA1 - USART0 RX - P34
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32MG13 Starter Kit (BRD4001A)
+Device: EFR32MG13P632F512GM48
+PA0 - USART0 TX - EXP Header Pin 12
+PA1 - USART0 RX - EXP Header Pin 14
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32BG13 Starter Kit (BRD4104A)
+Device: EFR32BG13P632F512GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32FG13 Starter Kit (BRD4256A)
+Device: EFR32FG13P233F512GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32MG14 Starter Kit (BRD4169A)
+Device: EFR32MG14P733F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32BG14 Starter Kit (BRD4105A)
+Device: EFR32BG14P732F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFR32FG14 Starter Kit (BRD4258A)
+Device: EFR32FG14P233F256GM48
+PA0 - USART0 TX - P9
+PA1 - USART0 RX - P11
+PF6 - Push Button PB0
+
+Board: Silicon Labs EFM32TG11 Starter Kit (SLSTK3301A)
+Device: EFM32TG11B520F128GM80
+PC0 - USART0 TX - EXP Header Pin 3
+PC1 - USART0 RX - EXP Header Pin 5
+PD5 - Push Button PB0
---
+++ series1/usart/gpio_prs_ldma_usart/src/main_s1_gg11.c
@@ -0,0 +1,167 @@
+/**************************************************************************//**
+ * @main_s1_GG11.c
+ * @brief This example demonstrates using LDMA with PRS where USART packet
+ * transfers are triggered every push of Push Button 0..
+ * @version 0.0.1
+ ******************************************************************************
+ * @section License
+ * <b>Copyright 2019 Silicon Labs, Inc. http://www.silabs.com</b>
+ *******************************************************************************
+ *
+ * This file is licensed under the Silabs License Agreement. See the file
+ * "Silabs_License_Agreement.txt" for details. Before using this software for
+ * any purpose, you must agree to the terms of that agreement.
+ *
+ ******************************************************************************/
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_usart.h"
+#include "em_ldma.h"
+#include "em_prs.h"
+#include "bsp.h"
+
+#define TX_LDMA_CHANNEL 1
+#define TXBUFFER_SIZE   	 7
+
+volatile char TxBuffer[20] = "Hello!\n";
+
+// Descriptor and config for the LDMA operation for sending data
+static LDMA_TransferCfg_t ldmaTXConfig;
+static LDMA_Descriptor_t descriptors[4];
+
+/**************************************************************************//**
+ * @brief Initialize descriptors array
+ *****************************************************************************/
+void initDescriptors(void) {
+
+	// Memory to peripheral transfer, Source: TxBuffer, Destination: USART0->TXDATA, Bytes to receive: 1
+	descriptors[0] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(TxBuffer, &(USART0->TXDATA), 1, 1);
+	descriptors[0].xfer.doneIfs = 0;
+
+	// Setting USART0 TXC flag to trigger LDMA transfers using PRS signals
+	descriptors[1] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_WRITE(((PRS_CH_CTRL_SOURCESEL_USART0 & _PRS_CH_CTRL_SOURCESEL_MASK) |
+																		(PRS_CH_CTRL_SIGSEL_USART0TXC & _PRS_CH_CTRL_SIGSEL_MASK) |
+																		(uint32_t)prsEdgePos << _PRS_CH_CTRL_EDSEL_SHIFT),
+																		&PRS->CH[0].CTRL,
+																		1);
+
+	// Memory to peripheral transfer, Source: TxBuffer, Destination: USART0->TXDATA, Bytes to receive: TXBUFFER_SIZE - 1
+	descriptors[2] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_M2P_BYTE(&TxBuffer[1], &(USART0->TXDATA), TXBUFFER_SIZE-1, 0);
+	descriptors[2].xfer.decLoopCnt = 1;
+
+	// Setting GPIO pin 8 to trigger LDMA transfers using PRS signals again
+	descriptors[3] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_WRITE(((PRS_CH_CTRL_SOURCESEL_GPIOH & _PRS_CH_CTRL_SOURCESEL_MASK) |
+																		(PRS_CH_CTRL_SIGSEL_GPIOPIN8 & _PRS_CH_CTRL_SIGSEL_MASK) |
+																		(uint32_t)prsEdgePos << _PRS_CH_CTRL_EDSEL_SHIFT),
+																		&PRS->CH[0].CTRL,
+																		1);
+}
+
+/**************************************************************************//**
+ * @brief Initialize USART0
+ *****************************************************************************/
+void initUSART0 (void)
+{
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	CMU_ClockEnable(cmuClock_USART0, true);
+
+	// Configure GPIO mode
+	// set pin modes for USART TX and RX pins
+	GPIO_PinModeSet(gpioPortC, 1, gpioModeInput, 0);
+	GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 1);
+
+
+	// Start with default config
+	USART_InitAsync_TypeDef config = USART_INITASYNC_DEFAULT;
+
+	// Init USART
+	USART_InitAsync(USART0, &config);
+
+	// Set USART pin locations
+	USART0->ROUTELOC0 = USART_ROUTELOC0_RXLOC_LOC5 | USART_ROUTELOC0_TXLOC_LOC5;
+	// Enable USART pins
+	USART0->ROUTEPEN |= USART_ROUTEPEN_TXPEN | USART_ROUTEPEN_RXPEN;
+
+
+	// Enable push button 0 as input
+	GPIO_PinModeSet(BSP_GPIO_PB0_PORT, BSP_GPIO_PB0_PIN, gpioModeInputPullFilter, 1);
+
+	// Enable push button 0 to generate interrupt signals so it can supply a signal to our prs
+	// but we turn off all of it's normal interrupt modes to avoid unwanted interrupt triggers
+	GPIO_IntConfig(BSP_GPIO_PB0_PORT, BSP_GPIO_PB0_PIN, 0, 0, false);
+
+	// Clear pending gpio interrupts
+	NVIC_ClearPendingIRQ(GPIO_EVEN_IRQn);
+	GPIO_IntClear(0x00FF);
+
+	// Using GPIO pin 8 to trigger LDMA transfers using PRS signals
+	CMU_ClockEnable(cmuClock_PRS, true);
+	PRS_SourceSignalSet(0, PRS_CH_CTRL_SOURCESEL_GPIOH,
+					  PRS_CH_CTRL_SIGSEL_GPIOPIN8, prsEdgePos);
+
+	// Configuring the PRS to send the PRS signal to the LDMA Channel 0
+	PRS->DMAREQ0 = PRS_DMAREQ0_PRSSEL_PRSCH0;
+
+	// Enable USART0
+	USART_Enable(USART0, usartEnable);
+}
+
+/**************************************************************************//**
+ * @IRQ Handler to reset transfers
+ *****************************************************************************/
+void LDMA_IRQHandler()
+{
+	LDMA_StartTransfer(TX_LDMA_CHANNEL, &ldmaTXConfig, &descriptors[0]);
+	LDMA_IntClear(1 << TX_LDMA_CHANNEL);
+	USART_IntClear(USART0, USART_IntGet(USART0));
+}
+
+/**************************************************************************//**
+ * @Initialize LDMA Descriptors
+ *****************************************************************************/
+void initLDMA(void)
+{
+	CMU_ClockEnable(cmuClock_LDMA, true);
+	LDMA_Init_t ldmaInit = LDMA_INIT_DEFAULT;
+	LDMA_Init(&ldmaInit); // Initializing default LDMA settings
+
+	// One byte will transfer everytime the USART TXBL flag is high
+	ldmaTXConfig = (LDMA_TransferCfg_t)LDMA_TRANSFER_CFG_PERIPHERAL(ldmaPeripheralSignal_PRS_REQ0);
+
+	// Starting both ldma transfers are different channels
+	LDMA_StartTransfer(TX_LDMA_CHANNEL, &ldmaTXConfig, &descriptors[0]);
+}
+
+/**************************************************************************//**
+ * @brief Transmitting TxBuffer
+ *****************************************************************************/
+void transmitTxBuffer(void)
+{
+  for(int i = 0; i < TXBUFFER_SIZE; i++)
+	  USART_Tx(USART0, TxBuffer[i]);
+}
+
+/**************************************************************************//**
+ * @brief Main function
+ *****************************************************************************/
+int main(void)
+{
+	// Initialize chip
+	CHIP_Init();
+
+	// Initialize USART0 as SPI slave
+	initUSART0();
+
+	// Initialize descriptors
+	initDescriptors();
+
+	// Setup LDMA channels for transfer across SPI
+	initLDMA();
+
+	transmitTxBuffer();
+
+	while(1);
+}
