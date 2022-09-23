df67512b2987c21a419c6bd7b316623a97873a1b
Manasa Rao <Manasa.Rao@silabs.com>
2022-09-23 20:46:01 +0000

    Increases data transfer size (Github Issue #121) and removes software CRC calculation in Series 1 GPCRC DMA example

--- series1/gpcrc/gpcrc_dma/readme.txt
+++ series1/gpcrc/gpcrc_dma/readme.txt
@@ -1,32 +1,35 @@
 gpcrc_dma
 
 This project demonstrates the GPCRC used to check 32-bit data using the 
-IEEE 802.3 polynomial standard in EM1.  Data is fed into and read out
-of the GPCRC via the LDMA.
+IEEE 802.3 polynomial standard in EM1. Data is fed into the GPCRC via the LDMA.
+The size of the data array being fed into LDMA and GPCRC can be increased 
+depending on the application. The example processes 16 words but can be expanded
+to process more data as needed. 
 
-Functionality is included to show how one could perform this conversion 
-without the GPCRC.  Of note, the pure software method requires more memory 
-and can take 3-5 times longer to compute.
+Using the GPCRC with LDMA has two benefits:
 
+It does not tie up the CPU. Once the LDMA descriptors are programmed and the 
+selected channel is started, the full data array is fed to the GPCRC without any 
+CPU intervention.
+Execution is deterministic. The LDMA always feeds an entire array into the GPCRC, 
+and this is going to have a set execution time (unless other bandwidth intensive 
+LDMA traffic is present).
 
-Note: We use LDMA_DESCRIPTOR_LINKREL_M2M_WORD() here even though we are
-performing transfer to/from a peripheral instead of just between memory.
-We are able to do this because our transfers are of size 1, so most of the
-differences between M2M and M2P transfers end up being irrelevant.  At the 
-time of writing this example, there is no LDMA_DESCRIPTOR_XXX that does exatly 
-what we want, so we'll have to use one that's close and change the values that 
-don't fit our desired use case.
+Using the GPCRC peripheral also greatly reduces the software overhead required 
+to calculate CRC32. 
 
+The GPCRC can also be used to blank check flash pages. This example (along with
+benchmarking data) is available here - https://github.com/SiliconLabs/platform_applications/tree/master/platform_gpcrc_blank_check 
 
 How To Test:
 1. Update the kit's firmware from the Simplicity Launcher (if necessary)
 2. Build the project and download to the Starter Kit
-3. Open the Simplicity Debugger and add "result" to the Expressions window
-4. Add a breakpoint at the beginning of LDMA_IRQHandler()
-5. Run the debugger.  You should see it pause inside the LDMA handler with
-a "checked" value inside "result"
-
+3. Open the Simplicity Debugger and add "crcResult" and "FIBONACCI_CRC_16WORDS" 
+to the Expressions window
+4. Add a breakpoint at the beginning of crcCheckResult()
+5. Run the debugger. You should see it the same values in crcResult and 
+FIBONACCI_CRC_16WORDS.  
 
 Peripherals Used:
-HFRCO  - 19 MHz
+HFRCO - 19 MHz
 GPCRC - IEEE 802.3 poly standard
--- series1/gpcrc/gpcrc_dma/src/main.c
+++ series1/gpcrc/gpcrc_dma/src/main.c
@@ -41,85 +41,57 @@
 #include "em_ldma.h"
 #include "em_emu.h"
 
+#define PRESET      0xFFFFFFFFUL
+/*
+ * This data was obtained from this online CRC calculator - https://crccalc.com/
+ * The CRC is calculated with the input data shown below -
+ * 0x00000001, 0x00000001, 0x00000002, 0x00000003, 0x00000005, 0x00000008
+ * 0x0000000D, 0x00000015, 0x00000022, 0x00000037, 0x00000059, 0x00000090,
+ * 0x000000E9, 0x00000179, 0x00000262, 0x000003DB
+ *
+ * However, the data is stored least significant byte first in RAM, which will
+ * change the input data to the GPCRC. Therefore, the input to the GPCRC is byte
+ * reversed so that CRC is calculated for the input shown above instead of
+ * 0x01000000, ...., 0xDB030000.
+ *
+ * As a part of post processing in the crc calculator shown above, the resulting
+ * CRC is XOR'd with 0xFFFFFFFF. This is not done by the GPCRC module on the
+ * device. Therefore, the CRC of the data is XOR'd with 0xFFFFFFFF before
+ * comparing it with the result of the online calculator.
+ */
+// IEEE 802.3 CRC for the input fibonacci data
+#define FIBONACCI_CRC_16WORDS (0x5CBF42AAUL)
 
-/* The width of the CRC calculation and result.
- * Modify the typedef for a 16 or 32-bit CRC standard. */
-typedef uint32_t crc_t;
-
-
-#define WIDTH       (8 * sizeof(crc_t))
-#define TOPBIT      (1 << (WIDTH - 1))
-#define POLYNOMIAL  0x04C11DB7
-#define PRESET      0xFFFFFFFF
 
 // DMA channel used
 #define LDMA_CHANNEL        0
 #define LDMA_CH_MASK        1 << LDMA_CHANNEL
 
 // Descriptor linked list for LDMA transfer
-LDMA_Descriptor_t descLink[2];
+LDMA_Descriptor_t ldmaCrcDesc[2];
+
+// store 16 words (16 x 4 = 64 bytes) in RAM
+#define ARRAY_SIZE 16
 
 // Arbitrary values to cycle through
-uint32_t data[4] = {0, 1, 2, 3};
+uint32_t data[ARRAY_SIZE];
 
-volatile uint32_t   source;
-volatile uint32_t   result;
-volatile uint32_t   i;
+// LDMA GPCRC channel assignment
+#define LDMA_GPCRC_CHAN  0
 
-volatile crc_t      crcTable[256];
+// Register bit clear address for LDMA_IEN
+#define LDMA_IEN_CLEAR  (((uint32_t)&(LDMA->IEN)) + 0x04000000)
 
-
-/***************************************************************************//**
- * @brief
- *   Initialize software ctcTable array for fast translations.
- ******************************************************************************/
-void initSoft(void)
+/**************************************************************************//**
+ * @brief  Calculates the n-th Fibonacci number recursively.
+ *****************************************************************************/
+uint32_t fib(uint32_t n)
 {
-  crc_t  remainder;
-
-  // Compute the remainder of each possible dividend
-  for (int i = 0; i < 256; ++i)
+  if (n < 2)
   {
-    // Start with the dividend followed by zeros
-    remainder = i << (WIDTH - 8);
-
-    // Perform modulo-2 division, a bit at a time
-    for (uint8_t bit = 8; bit > 0; --bit)
-    {
-      // XOR with polynomial if top bit is 1
-      if (remainder & (uint32_t)TOPBIT)
-        remainder = (remainder << 1) ^ POLYNOMIAL;
-      else
-        remainder = (remainder << 1);
-    }
-    // Store the result into the table
-    crcTable[i] = remainder;
+    return 1;
   }
-}
-
-/***************************************************************************//**
- * @brief
- *   Software CRC calculation function
- ******************************************************************************/
-crc_t softCrc(crc_t message, crc_t preset)
-{
-  uint8_t data, thisByte;
-  crc_t remainder = preset;
-
-  // Divide the message by the polynomial, a byte at a time
-  for (int byte = 0; byte < sizeof(crc_t); ++byte)
-  {
-    // Separate out one byte of the message
-    thisByte = (message >> (8*byte)) & 0x0FF;
-
-    // XOR this byte with the current remainder
-    data = thisByte ^ (remainder >> (WIDTH - 8));
-
-    // Set new remainder
-    remainder = crcTable[data] ^ (remainder << 8);
-  }
-  // The final remainder is the CRC
-  return (remainder);
+  return (fib(n-1) + fib(n-2));
 }
 
 /***************************************************************************//**
@@ -128,25 +100,8 @@ crc_t softCrc(crc_t message, crc_t preset)
  ******************************************************************************/
 void LDMA_IRQHandler( void )
 {
-  uint32_t pending;
-
-  // Read interrupt source
-  pending = LDMA_IntGet();
-
-  // Check for LDMA error
-  if ( pending & LDMA_IF_ERROR )
-  {
-    // Loop here to enable the debugger to see what has happened
-    while (1);
-  }
-
-  // Clear interrupts
-  LDMA_IntClear(pending);
-
-  source = data[(++i)%4];
-
-  // Request next transfer
-  LDMA->SWREQ |= LDMA_CH_MASK;
+  // Shouldn't ever get in here, so take a breakpoint
+  __BKPT(0);
 
 }
 
@@ -156,26 +111,14 @@ void LDMA_IRQHandler( void )
  ******************************************************************************/
 void initLdma(void)
 {
-
+  /*
+   * LDMA module initialization.  Note that the LDMA_IF_ERROR interrupt
+   * is also enabled by LDMA_Init(), so it's effectively mandatory to
+   * have LDMA_IRQHandler() because of this just to catch the error
+   * interrupt, if it happens.
+   */
   LDMA_Init_t init = LDMA_INIT_DEFAULT;
   LDMA_Init( &init );
-
-  // Use looped peripheral transfer configuration macro
-  LDMA_TransferCfg_t transferConfig = LDMA_TRANSFER_CFG_MEMORY();
-
-  // LINK descriptor macros for looping, SINGLE descriptor macro for single transfer
-  // See README for explanation of why a "M2M" (Memory to memory) transfer is used here instead
-  // of a M2P (Memory to Peripheral) or P2M (Peripheral to Memory)
-  descLink[0] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_M2M_WORD(&source, &(GPCRC->INPUTDATA), 1, 1);
-  descLink[1] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_M2M_WORD(&(GPCRC->DATA), &result, 1, -1);
-
-  // Enable interrupt after second descriptor
-  descLink[1].xfer.doneIfs = true;
-
-  // Disable automatic triggers for first descriptor
-  descLink[0].xfer.structReq = false;
-
-  LDMA_StartTransfer(LDMA_CHANNEL, (void*)&transferConfig, (void*)&descLink);
 }
 
 /**************************************************************************//**
@@ -186,20 +129,93 @@ void initGpcrc (void)
   // Enable clocks required
   CMU_ClockEnable(cmuClock_GPCRC, true);
 
-  // Declare init structs
+  // GPCRC module initialization for IEEE 802.3 polynomial
   GPCRC_Init_TypeDef init = GPCRC_INIT_DEFAULT;
-
   // Starting value in GPCRC_DATA
   init.initValue = PRESET;
-
   // Reset GPCRC_DATA to 0xFFFF_FFFF after every read
   init.autoInit = true;
+  // Reverse all bytes of the incoming message
+  init.reverseByteOrder = true;
 
   // Initialize GPCRC
   GPCRC_Init(GPCRC, &init);
+}
 
-  // Reset out GPCRC_DATA
+/**************************************************************************//**
+ * @brief  Uses LDMA to transfer the array data into GPCRC->INPUTDATA register
+ *****************************************************************************/
+void crcCheckStart (uint32_t *inputData)
+{
+  // Prep the GPCRC
   GPCRC_Start(GPCRC);
+
+  // Use the generic memory-to-memory transfer configuration; halt during debug
+  LDMA_TransferCfg_t transferConfig = LDMA_TRANSFER_CFG_MEMORY();
+  transferConfig.ldmaDbgHalt = true;
+
+  /*
+   * Even though the GPCRC is a peripheral, it's necessary to use the
+   * M2M descriptor because there is no DMA request (such as RXDATAV
+   * upon USART reception).  Instead, the LDMA simply needs to pump
+   * the page of flash through the GPCRC, word by word, as fast as it
+   * can manage to do so.
+   *
+   * One minor inconvenience here is that even when the descriptor's
+   * doneIfs field is set to 0 in order to avoid an interrupt upon
+   * channel completion, the LDMA always sets the corresponding
+   * LDMA_IF flag after executing the last descriptor.
+   *
+   * Setting the LDMA_IF flag in and of itself isn't a problem save
+   * for the fact that LDMA_StartTransfer() enables a channel's
+   * interrupt no matter what.
+   *
+   * Because the CRC blank checking operation is intended to run in
+   * this example until software checks to see if it is actually
+   * done, a little bit of trickery is used to disable the interrupt
+   * right after LDMA_StartTransfer() enables it.
+   *
+   * Instead of using a single descriptor to execute the GPCRC
+   * operation, a two element linked list is used.  The first entry
+   * in the list is a WRI descriptor that writes the bit mask for the
+   * GPCRC LDMA channel to the LDMA_IEN register in the peripheral
+   * bit clear aliasing region (register address + 0x04000000).  The
+   * next descriptor is the M2M transfer that feeds the flash page
+   * contents into the GPCRC_INPUTDATA register word by word.
+   */
+
+  ldmaCrcDesc[0] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_LINKREL_WRITE((1 << LDMA_GPCRC_CHAN), LDMA_IEN_CLEAR, 1);
+  ldmaCrcDesc[1] = (LDMA_Descriptor_t)LDMA_DESCRIPTOR_SINGLE_M2M_WORD(inputData, &(GPCRC->INPUTDATA), ARRAY_SIZE);
+  ldmaCrcDesc[1].xfer.dstInc = ldmaCtrlDstIncNone;
+
+  // This starts the CRC calculation.
+  LDMA_StartTransfer(LDMA_CHANNEL, (void*)&transferConfig, (void*)&ldmaCrcDesc);
+}
+
+/**************************************************************************//**
+ * @brief  Check to wait till LDMA transfer is completed
+ *****************************************************************************/
+bool crcCheckTransferBusy(void)
+{
+  if (LDMA_TransferDone(LDMA_GPCRC_CHAN) == true)
+    return false;
+  else
+    return true;
+}
+
+/**************************************************************************//**
+ * @brief  Reads GPCRC DATA register and compares with IEEE 802.3 result
+ *****************************************************************************/
+bool crcCheckResult(void)
+{
+  // XOR the data register output with 0xFFFFFFFF to get the post-processed
+  // IEEE 802.3 result
+  uint32_t crcResult = GPCRC_DataRead(GPCRC)^PRESET;
+
+  if (crcResult == FIBONACCI_CRC_16WORDS)
+    return true;
+  else
+    return false;
 }
 
 /**************************************************************************//**
@@ -209,24 +225,36 @@ int main(void)
 {
   CHIP_Init();
 
+  bool crcCheck = false;
+
+  // Fill data array with fibonacci values
+  for (uint32_t i = 0; i < ARRAY_SIZE; i++){
+      data[i] = (fib(i));
+  }
+
   // Initialize GPCRC
   initGpcrc();
 
   // Initialize LDMA
   initLdma();
 
-  // Initialize array for software method
-  initSoft();
+  // Transfer the data using the LDMA to the GPCRC input
+  crcCheckStart(data);
 
-  // Set up global variables
-  i = 0;
-  source = data[i++];
+  // Wait LDMA transfer is in progress
+  while (crcCheckTransferBusy() == true);
 
-  // Request first transfer
-  LDMA->SWREQ |= LDMA_CH_MASK;
+  // Read the GPCRC output and compare with predetermined CRC
+  crcCheck = crcCheckResult();
+
+  if (!crcCheck){
+      // Halt if crcCheck fails
+      __BKPT(2);
+  }
 
   // Infinite loop
   while(1){
-    EMU_EnterEM1();
+      // Enter EM1 if CRC check passes
+      EMU_EnterEM1();
   }
 }
